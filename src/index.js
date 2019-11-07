704ab912ad92834c8fec4ba700e001a973807779
andie5 <andieasa@googlemail.com>
2019-11-07 20:19:19 +0000

    adding tweet buttons

--- src/index.js
+++ src/index.js
@@ -5,18 +5,55 @@ import "./index.css";
 function Tweet() {
   return (
     <div className="tweet">
-        <Avatar />
-        Tweet
+      <Avatar />
+      <div className="content">
+        <Author />
+        <Time />
+        <Message />
+      </div>
+      <div className="buttons">
+        <ReplyButton />
+        <RetweetButton />
+        <LikeButton />
+        <MoreOptionsButton />
+      </div>
     </div>
-  )
+  );
 }
 
 function Avatar() {
-    return (
-        <img src="https://gravatar.com/avatar/nothing" 
-        className="avatar"
-        alt="avatar" />
-    )
+  return (
+    <img
+      src="https://gravatar.com/avatar/nothing"
+      className="avatar"
+      alt="avatar"
+    />
+  );
 }
+
+function Message() {
+  return <div className="message">This is less than 140 characters</div>;
+}
+
+function Author() {
+  return (
+    <span className="author">
+      <span className="name">Your Name</span>
+      <span className="handle">@yourhandler</span>
+    </span>
+  );
+}
+
+const Time = () => <span className="time">3h ago</span>;
+
+const ReplyButton = () => <i className="fa fa-reply reply-button" />;
+
+const RetweetButton = () => <i className="fa fa-retweet retweet-button" />;
+
+const LikeButton = () => <i className="fa fa-heart like-button" />;
+
+const MoreOptionsButton = () => (
+  <i className="fa fa ellipsis-h more-options-button" />
+);
 
 ReactDOM.render(<Tweet />, document.querySelector("#root"));
